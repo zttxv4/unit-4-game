3f837dd779dc9d819ea6fc3a19a06b2962f72c85
zttxv4 <zachttaylor00@gmail.com>
2019-09-27 21:29:56 -0500

    html and js started

---
+++ assets/game.js
@@ -0,0 +1,19 @@
+// Assign number values to each crystal picture
+var redCrys = 9
+var blueCrys = 13
+var greenCrys = 3
+var whiteCrys = 10
+var ComputerNumber = Math.floor(Math.random) * 100
+
+// Your running score
+
+var count = 0
+$("#yourScore").text(count)
+
+// Win/loss count
+
+var winCount = 0
+$("#winCount").text(winCount)
+
+var lossCount = 0
+$("lossCount").text(lossCount)
